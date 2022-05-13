39aacfcf197a2733b34d8348cfa398c505f3147c
Zalexanninev15 <blue.shark@disroot.org>
2022-05-13 14:44:52 +0300

    Изучил кортежи

---
+++ cortegi/target/debug/deps/cortegi-93b8970ef7b1b0a9.d
@@ -0,0 +1,5 @@
+e:\Github\MyRust\cortegi\target\debug\deps\cortegi-93b8970ef7b1b0a9.rmeta: src\main.rs
+
+e:\Github\MyRust\cortegi\target\debug\deps\cortegi-93b8970ef7b1b0a9.d: src\main.rs
+
+src\main.rs:
