4b2ae02040680c9348e9dc2f3ce9e9d8ede378f4
Zalexanninev15 <blue.shark@disroot.org>
2022-05-13 14:47:05 +0300

    Unnecessary

--- cortegi/target/debug/deps/cortegi-0aa3807dd2403cef.d
+++
@@ -1,5 +0,0 @@
-e:\Github\MyRust\cortegi\target\debug\deps\cortegi-0aa3807dd2403cef.rmeta: src\main.rs
-
-e:\Github\MyRust\cortegi\target\debug\deps\cortegi-0aa3807dd2403cef.d: src\main.rs
-
-src\main.rs:
