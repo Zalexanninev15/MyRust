25d5a705a77e99f74158f92a5ff6e3044d16260e
Zalexanninev15 <blue.shark@disroot.org>
2022-09-16 10:42:31 +0300

    HashMap, работа со строками, rust-cargo → basics

--- stroki/src/main.rs
+++ stroki/src/main.rs
@@ -1,7 +1,43 @@
 fn main() {
     let s1 = String::new(); // Создание строки
-    let s2 = String::from("Hello"); // Строка с значением (1 способ)
+    let mut s2 = String::from("Hello"); // Строка с значением (1 способ)
     let mut s3 = "Rust".to_string(); // Строка со значеним (2 способ)
     s3.push_str(" World!"); // Добавление к string
     println!("{}", s3);
+    s2.push('!'); // Добавление char к string
+    println!("{}", s2);
+    let mut res = s3 + &s2; // Соединение строк
+                            // или format!("{} {}", s3, s2);
+    println!("{}", res);
+
+    // &str - строковый литерал
+
+    // "Индексирование строк" (нарезка строк)
+    let mut hello = "Hello".to_string();
+    println!("Size: {}", hello.len()); // Размер строки, 1 символ АНГЛ = 1 байт
+    println!("{}", &hello[..1]); // H из Hello
+    hello = "Привет".to_string();
+    println!("Размер: {}", hello.len()); // Размер строки, 1 символ РУС = 1 байт
+    println!("{}", &hello[..2]); // П из Привет
+
+    // Перебор строк
+    let s_char = "TEXT".to_string();
+	
+    // 1 способ - по символам
+    for ch in s_char.chars() {
+        print!("{} ", ch);
+    }
+    println!("");
+	
+    // 2 способ - по байтам
+    for b in s_char.bytes() {
+        print!("{} ", b);
+    }
+	
+    // Байты для русского языка
+    let ru_s_char = "ТЕКСТ".to_string();
+    println!("");
+    for b in ru_s_char.bytes() {
+        print!("{} ", b);
+    }
 }
